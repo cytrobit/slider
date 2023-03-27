05550f4bbff7c3e69fef0d04ba674ec7fcde444a
cytrobit <cytrobit@gmail.com>
2023-03-27 10:02:55 +0200

    nice pistures added to array of objects + sentences

---
+++ slider.js
@@ -0,0 +1,25 @@
+let counter = 0;
+
+const data = [
+  {
+    sentence: 'No matter what...',
+    photo: 'https://images.pexels.com/photos/789296/pexels-photo-789296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    sentence: 'No matter where...',
+    photo: 'https://images.pexels.com/photos/5259419/pexels-photo-5259419.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    sentence: 'No matter with whom...',
+    photo: 'https://images.pexels.com/photos/950902/pexels-photo-950902.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    sentence: 'Enjoy your life',
+    photo: 'https://images.pexels.com/photos/3115637/pexels-photo-3115637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    sentence: 'And be yourself :)',
+    photo: 'https://images.pexels.com/photos/3812738/pexels-photo-3812738.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  }
+];
+
